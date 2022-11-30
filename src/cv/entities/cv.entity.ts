98b029edd8467c8beef74ef8d8a8bbad1f667a43
adamlahbib <adem.lahbib@insat.ucar.tn>
2022-11-30 23:33:59 +0100

    initial

---
+++ src/cv/entities/cv.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, JoinTable, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Skill } from "src/skill/entities/skill.entity";
+import { User } from "src/user/entities/user.entity";
+
+@Entity('cv')
+export class Cv {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    firstname: string;
+
+    @Column()
+    age: string;
+
+    @Column()
+    cin: string;
+
+    @Column()
+    occupation: string;
+
+    @Column()
+    path: string;
+
+    @JoinTable()
+    @ManyToOne(() => User, user => user.cvs, { eager: true, cascade: true })
+    user: User;
+
+    @JoinTable()
+    @ManyToOne(() => Skill, skill => skill.cvs, { eager: true, cascade: true })
+    skills: Skill[];
+
+}
